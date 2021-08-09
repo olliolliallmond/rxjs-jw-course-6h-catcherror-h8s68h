75967e025c2da1cb871502b01ac4dcaa6e5d9a76
olliolliallmond <aallmondcar@gmail.com>
2021-08-09 14:53:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-6h-catcherror-h8s68h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-6h-catcherror-h8s68h)
---
+++ index.ts
@@ -0,0 +1,19 @@
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const failingHttpRequest$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error(new Error('Timeout'));
+  }, 3000);
+});
+
+console.log('Starting app...');
+
+// failingHttpRequest$
+//   .pipe(catchError(error => of('Fallback value')))
+//   .subscribe(value => console.log(value));
+
+failingHttpRequest$.pipe(catchError(error => EMPTY)).subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed')
+});
